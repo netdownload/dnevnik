22831d78374f0f7348c2fb656eafe8e2f0b7c03c
netdownload <netdownload@yandex.ru>
2022-03-22 16:35:26 +0300

    get  json from script

--- dnevnik.py
+++ dnevnik.py
@@ -1,7 +1,10 @@
 from gettext import find
+from operator import le
 import requests
-import secret
+import json
 from bs4 import BeautifulSoup
+
+import secret
 
 class Dnevnik:
     
@@ -12,9 +15,13 @@ class Dnevnik:
     
     def get_name(self):
         r = requests.post('https://login.dnevnik.ru/login/', data={"login": self.login, "password": self.password})
-        soup = BeautifulSoup(r.content, 'html.parser')
-        name = soup.find('div', class_ = 'user-profile-box'). find('p', class_ = 'user-profile-box__info_row-content user-profile-box__initials').text
-        return name
+        soup = BeautifulSoup(r.content, 'lxml')
+        
+        # name = soup.find('div', class_ = 'user-profile-box').find('p', class_ = 'user-profile-box__info_row-content user-profile-box__initials').text
+        find_in_script = soup.find_all('script')
+        to_json = json.loads(find_in_script[27].string[250:-3700])
+        print(to_json['userMarks']['children'][0]['schoolId'])
+        return None
         
 dnevnik = Dnevnik(secret.LOGIN, secret.PASSWORD)
 print(dnevnik.get_name())
